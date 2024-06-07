ffae149fbaee0a102249d7e9e2fde2c8821eb225
heywb456789 <heywb457@naver.com>
2024-06-07 16:15:59 +0900

    class 학습중

---
+++ class.dart
@@ -0,0 +1,136 @@
+class Player {
+  final String name = 'minjae';
+  int xp = 1500;
+
+  void hello() {
+    print('$name');
+  }
+}
+
+//constructor
+class Player2 {
+  late final String name;
+  late int xp;
+
+  Player2(String name, int xp) {
+    this.name = name;
+    this.xp = xp;
+  }
+
+  void hello() {
+    print('hi $name');
+  }
+}
+
+//improve
+class Player3 {
+  late final String name;
+  late int xp;
+
+  Player3(this.name, this.xp);
+
+  void hello() {
+    print('hi $name');
+  }
+}
+
+//named Constructor
+class Player4 {
+  final String name;
+  String team;
+  int age;
+
+  Player4({
+    required this.name,
+    required this.team,
+    required this.age,
+  });
+
+  Player4.createBluePlaer({required String team, required int age})
+      : this.team = team,
+        this.age = age,
+        this.name = 'minjae';
+}
+
+class Player5 {
+  final String name;
+  int xp;
+  String team;
+
+  Player5.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void helloss() {
+    print("$name , $team, $xp");
+  }
+}
+
+class Player6 {
+  String name;
+  int xp;
+  Team team;
+
+  Player6({required this.name, required this.xp, required this.team});
+}
+
+//enum
+enum Team { red, blue }
+
+//abstractMethod
+abstract class Human {
+  void walk();
+}
+
+class Robot {
+  final String name;
+  Robot(this.name);
+  void sayHello() {
+    print("hi myname $name");
+  }
+}
+
+class chidRobot extends Robot {
+  final Team team;
+
+  chidRobot({required this.team, required String name}) : super(name);
+}
+
+void main() {
+  var palyer = new Player2("minjae", 1500);
+  palyer.hello();
+
+  var palyer3 = new Player3("minjae", 1500);
+  palyer3.hello();
+
+  // palyer.name = 'minjae2'; // error
+
+  var player4 = new Player4(name: "minjae", age: 12, team: "blue");
+
+  var palyer5 = new Player4.createBluePlaer(team: "blue", age: 11);
+
+  var apiData = [
+    {
+      "name": "minjae",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "minjae",
+      "team": "red",
+      "xp": 0,
+    }
+  ];
+
+  apiData.forEach((playerJson) {
+    var player6 = Player5.fromJson(playerJson);
+    player6.helloss();
+  });
+
+//cascadeOperator
+  var minjae = Player6(name: 'minjae', xp: 1, team: Team.blue)
+    ..name = 'change'
+    ..xp = 12
+    ..team = Team.red;
+}
